4ea39dc2e57fe86c3abdea44222d38ee645ac319
Ernie Rimer <ernest.rimer@icloud.com>
2020-06-01 21:49:18 -0600

    validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ApplicationRecord
   belongs_to :rank
   has_many :songs, dependent: :destroy
+  validates :name, :genre, presence: true
+  validates :name, length: { minimum: 1 }
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
   belongs_to :artist
+  validates :name, :duration, presence: true
+  validates :duration, { only_integer: true }
+  validates :name, length: { minimum: 1 }
 end
